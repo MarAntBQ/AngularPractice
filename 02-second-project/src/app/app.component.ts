365242b33770313c699d37293dc8a39dfe374312
MarAntBQ <github@marantbq.dev>
2024-05-29 01:28:49 -0500

    Learning about Components

--- 02-second-project/src/app/app.component.ts
+++ 02-second-project/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent], // Import Components inside
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = '02-second-project';
   developer = {
