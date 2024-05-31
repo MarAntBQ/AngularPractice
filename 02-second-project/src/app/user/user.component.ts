69459833357f0d9a161b727afd29225c562cfaac
MarAntBQ <github@marantbq.dev>
2024-05-31 00:25:12 -0500

    sending an array of users to the component html and render

--- 02-second-project/src/app/user/user.component.ts
+++ 02-second-project/src/app/user/user.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-
+  users = DUMMY_USERS
 }
