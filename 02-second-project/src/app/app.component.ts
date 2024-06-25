b8e1427c00624e87c351b50e38b300128446e5c0
MarAntBQ <github@marantbq.dev>
2024-06-24 23:23:46 -0500

    Sending data to other controller with click

--- 02-second-project/src/app/app.component.ts
+++ 02-second-project/src/app/app.component.ts
@@ -2,19 +2,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
     selector: 'app-root',
     standalone: true, // Import Components inside
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [HeaderComponent, UserComponent]
+    imports: [HeaderComponent, UserComponent, TasksComponent]
 })
 
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId)!
+  }
 
   onSelectUser(id: string) {
-    console.log('User selected with id: ' + id)
+    this.selectedUserId = id;
   }
 }
