0e6160f741c379888e7cb08df2a8b9c639590a24
MarAntBQ <github@marantbq.dev>
2024-06-25 23:41:50 -0500

    Use object Type

--- 02-second-project/src/app/user/user.component.ts
+++ 02-second-project/src/app/user/user.component.ts
@@ -9,16 +9,18 @@ import { CommonModule } from '@angular/common';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
   @Output() select  = new EventEmitter();
 
   get imagePath(){
-    return `assets/users/${this.avatar}`;
+    return `assets/users/${this.user.avatar}`;
   }
 
   onSelectUser(){
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
